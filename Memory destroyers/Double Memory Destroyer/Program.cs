a4a37777ccffd6aa7e799892a99a2ff07a0235f6
SemkiShow <SemkiShow@github.com>
2023-03-12 17:36:22 +0300

    Upgraded the DoubleMemoryDestoryer

--- Memory destroyers/Double Memory Destroyer/Program.cs
+++ Memory destroyers/Double Memory Destroyer/Program.cs
@@ -7,28 +7,29 @@ namespace Memory_destroyers
     {
         // static DriveInfo di = new DriveInfo("c");
         static string drive;
-        static long memory;
+        static int memory;
         static void Main(string[] args)
         {
             Console.WriteLine("What drive do you want to destruct? (For example: if you want to destruct drive D - write D).");
             drive = Console.ReadLine();
-            memory = new DriveInfo(drive).TotalFreeSpace / 1000;
+            // memory = new DriveInfo(drive).TotalFreeSpace / 1000;
+	    Console.WriteLine("How many GibiBytes do you want to waste?")
+	    memory = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Convirm memory destruction pressing any key");
             Console.ReadKey();
-            for (int x = 1; x < memory / 30 + 1; x++)
+            for (int x = 0; x < memory; x++)
             {
-                if (File.Exists(drive + ":/Memory (" + Convert.ToString(x) + ").txt"))
+                if (File.Exists(drive + "/Memory (" + Convert.ToString(x) + ").txt"))
                 {
-                    File.Delete(drive + ":/Memory (" + Convert.ToString(x) + ").txt");
+                    File.Delete(drive + "/Memory (" + Convert.ToString(x) + ").txt");
                 }
-                FileStream file = File.Create(drive + ":/Memory (" + Convert.ToString(x) + ").txt");
+                FileStream file = File.Create(drive + "/Memory (" + Convert.ToString(x) + ").txt");
                 file.Close();
-                File.AppendAllText(drive + ":/Memory (" + Convert.ToString(x) + ").txt", "S");
-                for (var i = 1; i < 31; i++)
-                {
-                    Console.WriteLine(Convert.ToString(i) + "/30, " + Convert.ToString(x) + "/" + Convert.ToString(memory / 30));
-                    File.AppendAllText(drive + ":/Memory (" + Convert.ToString(x) + ").txt", File.ReadAllText(drive + ":/Memory (" + Convert.ToString(x) + ").txt"));
-                }
+		for (int i = 0; i < 1024; i++)
+		{	
+			Console.WriteLine(x + " GiB " + i + " MiB " + "[" + new string('#', Convert.ToInt16(Convert.ToDouble(x * 1024 + i) / (memory * 1024) * 100)) + new string ('.', Convert.ToInt16(100 - Convert.ToDouble(x * 1024 + i) / (memory * 1024) * 100)) + "] " + (Convert.ToInt16(Convert.ToDouble(x * 1024 + i) / (memory * 1024) * 100)) + "%");
+			File.AppendAllText(drive + "/Memory (" + Convert.ToString(x) + ").txt", new string('S', 1024 * 1024));
+		}
             }
         }
     }
